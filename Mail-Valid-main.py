c63323a9ff118d64fefc7dd6c4017a650ea2e094
localberkah <119092526+inxberkah@users.noreply.github.com>
2023-10-16 06:47:55 -0700

    Update Mail-Valid-main.py

--- Mail-Valid-main.py
+++ Mail-Valid-main.py
@@ -3,39 +3,17 @@ import urllib3
 import json
 import concurrent.futures
 
-# Input file path
-file_path = input("Input List: ")
-
-# Input jumlah thread yang diinginkan
-num_threads = int(input("Jumlah Thread: "))
-
-# Nama file untuk menyimpan email yang "live"
-output_file = input("Output File: ")
-
-# Input file path untuk daftar proxy
-proxy_file_path = input("File Proxy List (Format: proxy:port per baris): ")
-
 def load_proxies(proxy_file_path):
     with open(proxy_file_path, 'r') as proxy_file:
-        proxy_list = [line.strip() for line in proxy_file if line.strip()]
-    return proxy_list
+        return [line.strip() for line in proxy_file if line.strip()]
 
-# Load proxy list from file
-proxy_list = load_proxies(proxy_file_path)
-
-def process_email(email_input):
+def process_email(email_input, proxy_list, output_file):
     for proxy in proxy_list:
         try:
-            proxy_parts = proxy.split(':')
-            if len(proxy_parts) != 2:
-                print(f"Invalid proxy format: {proxy}")
-                continue
-
-            proxy_host = proxy_parts[0]
-            proxy_port = proxy_parts[1]
-
-            uid = requests.get('https://random-data-api.com/api/device/random_device', proxies={'http': f'http://{proxy_host}:{proxy_port}'}).json()['uid']
-
+            proxy_host, proxy_port = proxy.split(':')
+            
+            uid = requests.get('https://random-data-api.com/api/device/random_device', 
+                               proxies={'http': f'http://{proxy_host}:{proxy_port}'}).json()['uid']
 
             urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -50,9 +28,7 @@ def process_email(email_input):
 
             json_data = {
                 'action': 'STEP_ENTER__EMAIL__SUBMIT',
-                'context': {
-                    'value': '',
-                },
+                'context': {'value': ''},
                 'deviceContext': {
                     'aid': 337862,
                     'deviceId': uid,
@@ -73,8 +49,9 @@ def process_email(email_input):
                 json=json_data,
                 verify=False,
                 proxies={'http': f'http://{proxy_host}:{proxy_port}'},
-                timeout=5  # Timeout set to 5 seconds
+                timeout=5
             )
+
             js = json.loads(response.text)
 
             if 'STEP_REGISTER__PASSWORD' in response.text:
@@ -82,22 +59,24 @@ def process_email(email_input):
             elif 'IDENTIFIER_TYPE__EMAIL' in response.text:
                 if 'context' in js and 'value' in js['context']:
                     print(f"LIVE => {email_input}")
-                    # Simpan alamat email yang "live" ke dalam file teks
                     with open(output_file, 'a') as live_file:
                         live_file.write(email_input + '\n')
-                    break  # Jika berhasil, keluar dari loop proxy
+                    break
 
         except requests.exceptions.RequestException:
             print(f"Bad Proxy => {proxy}")
-            continue  # Jika proxy tidak merespons, coba proxy berikutnya
+            continue
 
 if __name__ == '__main__':
+    file_path = input("Input List: ")
+    num_threads = int(input("Jumlah Thread: "))
+    output_file = input("Output File: ")
+    proxy_file_path = input("File Proxy List (Format: proxy:port per baris): ")
+
+    proxy_list = load_proxies(proxy_file_path)
+
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures = [executor.submit(process_email, line.strip()) for line in lines]
-
-    # Mengumpulkan hasilnya jika diperlukan
-    # for future in concurrent.futures.as_completed(futures):
-    #     result = future.result()
+        futures = [executor.submit(process_email, line.strip(), proxy_list, output_file) for line in lines]
